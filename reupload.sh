7521b76ae6be78878dd6ba2d5cf01e053adc0af2
Gilles FRANCOIS <gilles@coworking-metz.fr>
2024-09-10 16:00:16 +0200

    Le script de reupload travaille maintenant sur une periode données, car il doit telecharger les logs depuis s3 avant de travailler.

--- reupload.sh
+++ reupload.sh
@@ -3,6 +3,7 @@
 BASE_DIR=$(realpath "$(dirname "$0")")
 
 MAC=$1
+PERIOD=$2
 
 echo "##################################################################"
 echo "# This script will find presences in the log for a given MAC     #"
@@ -12,30 +13,43 @@ echo "##################################################################"
 if [ -z "$MAC" ]; then
     # Prompt the user for the MAC address
     read -p "Please enter the MAC address: " MAC
-    # Check again if the MAC variable is empty after user input
     if [ -z "$MAC" ]; then
         echo "Error: No MAC address provided, halting the script."
         exit 1  # Exit with a non-zero status to indicate an error
     fi
 fi
 
+# Check if the MAC variable is empty
+if [ -z "$PERIOD" ]; then
+    # Prompt the user for the MAC address
+    echo "Please enter the period to reupload."
+    read -p "The period is either a whole year (formatted YYYY) or specific month (formatted YYYY-MM): " PERIOD
+    if [ -z "$PERIOD" ]; then
+        echo "Error: No period  provided, halting the script."
+        exit 1  # Exit with a non-zero status to indicate an error
+    fi
+fi
+
 source "${BASE_DIR}/base.sh"
 
-echo "Upload presences for MAC Address $MAC"
 
 MAC_DIR="${TMP_DIR}/MAC"
 mkdir -p "${MAC_DIR}"
 
-cd ${PROBES_DIR}
+echo "Fetching log files from s3"
+rclone sync --include "*/${PERIOD}*" ovh:coworking-metz/presences/ ${MACS_PROBES_DIR} --progress --update
 
+echo "Handling presences for MAC Address $MAC for $PERIOD"
+
+cd "${MACS_PROBES_DIR}"
 NB_LINES=0;
 for item in *; do
     # Check if the item is a directory
     if [ -d "$item" ]; then
         LOCATION="$item"
-        echo "  - Looking for presences in location $LOCATION"
+        echo "  - Looking for presences in location '$LOCATION'"
         
-        for PROBE in $(grep -rl "${MAC}" ${PROBES_DIR}/${LOCATION}/)
+        for PROBE in $(grep -rl "${MAC}" ${MACS_PROBES_DIR}/${LOCATION}/ | grep "$PERIOD")
         do
             DATE=$(basename "$PROBE")
             grep "${MAC}" "${PROBE}" >> "${MAC_DIR}/${DATE}"
@@ -49,7 +63,9 @@ if [ $NB_LINES -eq 0 ]; then
     exit 1
 fi
 
-echo "${NB_LINES} lines found in the log - Now calculating presences"
+echo " - ${NB_LINES} lines found in the log - Now calculating presences..."
+
+
 cd ${MAC_DIR}
 NB_PRESENCES=0
 for PROBE in *; do
@@ -62,10 +78,10 @@ for PROBE in *; do
         PAYLOAD="email=${EMAIL}&date=${DATE}&amount=${AMOUNT}"
         STATUS=$(curl -q -s -d "key=${TICKET_BACKEND_TOKEN}&${PAYLOAD}" "${TICKET_BACKEND_URL}/api/presence")
         if [ "$STATUS" = "OK" ]; then
-            echo "    - ${AMOUNT} day presence on ${DATE} for ${EMAIL} uploaded"
+            echo "  - ${AMOUNT} day presence on ${DATE} for ${EMAIL} uploaded"
             NB_PRESENCES=$(($NB_PRESENCES+1))
         else
-            echo "    - ERROR: Unable to upload ${AMOUNT} day presence on ${DATE} for ${EMAIL}"
+            echo "  - ERROR: Unable to upload ${AMOUNT} day presence on ${DATE} for ${EMAIL}"
         fi
     fi
     # grep "${MAC}" "${PROBE}" >> "${TMP_DIR}/MAC/${DATE}"
@@ -76,4 +92,16 @@ else
     echo "${NB_PRESENCES} presence(s) uploaded for the given MAC address"
 fi
 rm -rf "$MAC_DIR"
+
+echo "The script as ended"
+echo "Do you want to delete the temporary log files folder?"
+echo "This folder is only used for the reupload.sh script,"
+read -p "it takes some disk space and is not used by the daily upload.sh script (y/N) " response
+
+if [[ $response == "Y" || $response == "y" ]]; then
+    # Proceed with deleting the directory and its contents
+    rm -rf "$MACS_PROBES_DIR"
+    echo "Folder and contents deleted."
+fi
+
 exit 0
